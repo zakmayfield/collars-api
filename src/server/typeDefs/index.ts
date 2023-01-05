c3294bd506ac2cc269426de24d89c57b01a86b14
Zak Mayfield <zakmayfield@gmail.com>
2023-01-05 14:25:23 -0500

    created several types for new schema design

--- src/server/typeDefs/index.ts
+++ src/server/typeDefs/index.ts
@@ -20,7 +20,10 @@ export const typeDefs = `#graphql
         country: String!
         
         agencyProfile: AgencyProfile
-        volunteerProfile: VolunteerProfile
+        userProfile: UserProfile
+
+        agencyProfileId: Int
+        userProfileId: Int
     }
 
 
@@ -36,9 +39,9 @@ export const typeDefs = `#graphql
         email: String
 
         agencyProfile: AgencyProfile
-        agencyProfileId: Int
+        userProfile: UserProfile
 
-        volunteerProfile: VolunteerProfile
+        agencyProfileId: Int
         volunteerProfileId: Int
     }
 
@@ -56,8 +59,8 @@ export const typeDefs = `#graphql
         token: String
 
         profile: AgencyProfile
-        pets: [Pet]!
-        volunteers: [Volunteer]!
+        # pets: [Pet]!
+        volunteers: [User]!
     }
 
     type AgencyProfile {
@@ -65,124 +68,15 @@ export const typeDefs = `#graphql
         createdAt: String
         updatedAt: String
 
-        username: String
+        username: String!
         bio: String
         
 
         contact: Contact
-        contactId: Int
-
         address: Address
-        addressId: Int
 
         agency: Agency!
         agencyId: Int!
-    }
-
-
-
-
-    # ::: pet :::
-    type Pet {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        agency: Agency!
-        profile: PetProfile
-    }
-
-    type PetProfile {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        name: String!
-        age: Int
-        bio: String
-        species: String
-        breed: [BreedToPetProfile]
-        sex: String
-        weight: Int
-        birthday: String
-        coat: String
-        behavior: Behavior
-        isHouseTrained: Boolean
-        isAvailable: Boolean!
-        
-        color: [ColorToPetProfile]!
-        images: [PetImageToPetProfile]!
-        favoritedBy: [UserProfileToPetProfile]!
-        volunteers: [VolunteerProfileToPetProfile]!
-
-        pet: Pet!
-        petId: Int!
-    }
-
-    type Behavior {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        goodWith: String
-        avoid: String
-        personalityType: String
-
-        petProfile: PetProfile!
-        petProfileId: Int!
-    }
-
-    type PetImage {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        url: String
-        file: String
-        thumbnail: String
-
-        pets: [PetImageToPetProfile]
-    }
-
-    type PetImageToPetProfile {
-        petImage: PetImage!
-        petImageId: Int!
-        petProfile: PetProfile!
-        petProfileId: Int!
-    }
-
-    type Breed {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        name: String!
-
-        pets: [BreedToPetProfile]
-    }
-
-    type BreedToPetProfile {
-        breed: Breed!
-        breedId: Int!
-        petProfile: PetProfile!
-        petProfileId: Int!
-    }
-
-    type Color {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        color: String!
-
-        pets: [ColorToPetProfile]
-    }
-
-    type ColorToPetProfile {
-        color: Color!
-        colorId: Int!
-        petProfile: PetProfile!
-        petProfileId: Int!
     }
 
 
@@ -194,12 +88,17 @@ export const typeDefs = `#graphql
         createdAt: String
         updatedAt: String
 
-        username: String! 
+        username: String!
         email: String!
         password: String!
+        role: String!
         token: String
 
         profile: UserProfile
+        agency: Agency
+        savedPets: [UsersToPets]!
+
+        agencyId: Int
     }
 
     type UserProfile {
@@ -210,16 +109,79 @@ export const typeDefs = `#graphql
         firstName: String
         lastName: String
         bio: String
-        favorites: [UserProfileToPetProfile]!
+
+        contact: Contact
+        address: Address
 
         user: User!
         userId: Int!
     }
 
-    # favorites
-    type UserProfileToPetProfile {
-        userProfile: UserProfile!
-        userProfileId: Int!
+    type UsersToPets {
+        user: User!
+        userId: Int!
+        pet: Pet!
+        petId: Int!
+    }
+
+
+
+
+    # ::: pet :::
+    type Pet {
+        id: ID!
+        createdAt: String
+        updatedAt: String
+
+        name: String!
+        species: String!
+        breed: [BreedsToPets]!
+        savedBy: [UsersToPets]!
+
+        agency: Agency!
+        profile: PetProfile
+    }
+
+    type PetProfile {
+        id: ID!
+        createdAt: String
+        updatedAt: String
+
+        age: Int
+        bio: String
+        sex: String
+        weight: Int
+        birthday: String
+        coat: String
+        goodWith: String
+        personalityType: String
+        isHouseTrained: Boolean
+        isAvailable: Boolean!
+        
+        colors: [ColorsToPetProfiles]!
+        images: [ImagesToPetProfiles]!
+
+        pet: Pet!
+        petId: Int!
+    }
+
+
+
+
+    # ::: color :::
+    type Color {
+        id: ID!
+        createdAt: String
+        updatedAt: String
+
+        color: String
+
+        pets: [ColorsToPetProfiles]
+    }
+
+    type ColorsToPetProfiles {
+        color: Color!
+        colorId: Int!
         petProfile: PetProfile!
         petProfileId: Int!
     }
@@ -227,47 +189,235 @@ export const typeDefs = `#graphql
 
 
 
-    # ::: volunteer :::
-    type Volunteer {
+
+    # ::: breed :::
+    type Breed {
+        id: ID!
+        createdAt: String
+        updatedAt: String
+       
+
+        breed: String
+
+        pets: [BreedsToPets]
+    }
+
+    type BreedsToPets {
+        breed: Breed!
+        breedId: Int!
+        pet: Pet!
+        petId: Int!
+    }
+
+
+
+
+    # ::: image :::
+    type Image {
         id: ID!
         createdAt: String
         updatedAt: String
 
-        email: String!
-        password: String!
-        token: String
-
-        profile: VolunteerProfile
-
-        agency: Agency
-        agencyId: Int
+        url: String
+        file: String
+        thumbnail: String
     }
 
-    type VolunteerProfile {
-        id: ID!
-        createdAt: String
-        updatedAt: String
-
-        firstName: String
-        lastName: String
-        bio: String
-
-        contact: Contact
-        contactId: Int
-
-        address: Address
-        addressId: Int
-
-        assignedPets: [VolunteerProfileToPetProfile]!
-
-        volunteer: Volunteer!
-        volunteerId: Int!
+    type ImagesToPetProfiles {
+        image: Image!
+        imageId: Int!
+        petProfile: PetProfile!
+        petProfileId: Int!
     }
 
-    type VolunteerProfileToPetProfile {
-        volunteerProfile: VolunteerProfile
-        volunteerProfileId: Int
-        petProfile: PetProfile
-        petProfileId: Int
-    }
+
+    # -----------------------------
+    # # ::: pet :::
+    # type Pet {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     agency: Agency!
+    #     profile: PetProfile
+    # }
+
+    # type PetProfile {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     name: String!
+    #     age: Int
+    #     bio: String
+    #     species: String
+    #     breed: [BreedToPetProfile]
+    #     sex: String
+    #     weight: Int
+    #     birthday: String
+    #     coat: String
+    #     behavior: Behavior
+    #     isHouseTrained: Boolean
+    #     isAvailable: Boolean!
+        
+    #     color: [ColorToPetProfile]!
+    #     images: [PetImageToPetProfile]!
+    #     favoritedBy: [UserProfileToPetProfile]!
+    #     volunteers: [VolunteerProfileToPetProfile]!
+
+    #     pet: Pet!
+    #     petId: Int!
+    # }
+
+    # type Behavior {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     goodWith: String
+    #     avoid: String
+    #     personalityType: String
+
+    #     petProfile: PetProfile!
+    #     petProfileId: Int!
+    # }
+
+    # type PetImage {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     url: String
+    #     file: String
+    #     thumbnail: String
+
+    #     pets: [PetImageToPetProfile]
+    # }
+
+    # type PetImageToPetProfile {
+    #     petImage: PetImage!
+    #     petImageId: Int!
+    #     petProfile: PetProfile!
+    #     petProfileId: Int!
+    # }
+
+    # type Breed {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     name: String!
+
+    #     pets: [BreedToPetProfile]
+    # }
+
+    # type BreedToPetProfile {
+    #     breed: Breed!
+    #     breedId: Int!
+    #     petProfile: PetProfile!
+    #     petProfileId: Int!
+    # }
+
+    # type Color {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     color: String!
+
+    #     pets: [ColorToPetProfile]
+    # }
+
+    # type ColorToPetProfile {
+    #     color: Color!
+    #     colorId: Int!
+    #     petProfile: PetProfile!
+    #     petProfileId: Int!
+    # }
+
+
+
+
+    # # ::: user :::
+    # type User {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     username: String! 
+    #     email: String!
+    #     password: String!
+    #     token: String
+
+    #     profile: UserProfile
+    # }
+
+    # type UserProfile {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     firstName: String
+    #     lastName: String
+    #     bio: String
+    #     favorites: [UserProfileToPetProfile]!
+
+    #     user: User!
+    #     userId: Int!
+    # }
+
+    # # favorites
+    # type UserProfileToPetProfile {
+    #     userProfile: UserProfile!
+    #     userProfileId: Int!
+    #     petProfile: PetProfile!
+    #     petProfileId: Int!
+    # }
+
+
+
+
+    # # ::: volunteer :::
+    # type Volunteer {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     email: String!
+    #     password: String!
+    #     token: String
+
+    #     profile: VolunteerProfile
+
+    #     agency: Agency
+    #     agencyId: Int
+    # }
+
+    # type VolunteerProfile {
+    #     id: ID!
+    #     createdAt: String
+    #     updatedAt: String
+
+    #     firstName: String
+    #     lastName: String
+    #     bio: String
+
+    #     contact: Contact
+    #     contactId: Int
+
+    #     address: Address
+    #     addressId: Int
+
+    #     assignedPets: [VolunteerProfileToPetProfile]!
+
+    #     volunteer: Volunteer!
+    #     volunteerId: Int!
+    # }
+
+    # type VolunteerProfileToPetProfile {
+    #     volunteerProfile: VolunteerProfile
+    #     volunteerProfileId: Int
+    #     petProfile: PetProfile
+    #     petProfileId: Int
+    # }
 `;
