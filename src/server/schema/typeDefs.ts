15c2ce05cf04e2e60255894bf28a8b7d66ef60fd
Zak Mayfield <zakmayfield@gmail.com>
2023-02-16 16:53:26 -0500

    typeDefs.ts line 27 - 30

--- src/server/schema/typeDefs.ts
+++ src/server/schema/typeDefs.ts
@@ -1,25 +1,10 @@
 export const typeDefs = /* GraphQL */ `
   type Query {
     getUser: User!
-    # linkFeed(filterNeedle: String, take: Int, skip: Int): [Link]!
-    # link(id: Int!): Link
-    # comment(id: Int!): Comment
-    # linkComments(linkId: Int!): Link
   }
 
   type Mutation {
-    ## prevent post link if user has a link with the same title
-    # postLink(description: String!, url: String!): Link
-    # postCommentOnLink(linkId: Int!, body: String!): Comment
-    # deleteCommentOnLink(commentId: Int!): Comment
-    # updateCommentOnLink(commentId: Int!, body: String!): Comment
-
-    # deleteLink(id: Int!): Link!
-    # updateLink(id: Int!, description: String, url: String): Link!
-
     signUp(name: String!, email: String!, password: String!, type: String!): AuthPayload
-    # login(email: String!, password: String!): AuthPayload
-    # deleteUserAccount(password: String!): User
   }
 
   type AuthPayload {
@@ -39,10 +24,10 @@ export const typeDefs = /* GraphQL */ `
     name: String!
     email: String!
     type: String!
-    profile: UserProfile
-    pets: Pet[]
-    savedPets: UsersToPets[]
-    volunteers: Volunteer[]
+    # profile: UserProfile
+    # pets: Pet[]
+    # savedPets: UsersToPets[]
+    # volunteers: Volunteer[]
   }
 
   # enum AccountType {
@@ -51,128 +36,128 @@ export const typeDefs = /* GraphQL */ `
   #   AGENCY
   # }
 
-  type Volunteer {
-    id: ID!
-    name: String!
-    email: String!
-    address: Address
+#   type Volunteer {
+#     id: ID!
+#     name: String!
+#     email: String!
+#     address: Address
 
-    agency: User
-  }
+#     agency: User
+#   }
 
-  type UsersToPets {
-    user: User
-    pet: Pet
-  }
+#   type UsersToPets {
+#     user: User
+#     pet: Pet
+#   }
 
-  type Pet {
-    id: ID!
-    name: String!
-    species: String!
-    breed: BreedsToPets[]
-    savedBy: UsersToPets[]
-    profile: PetProfile?
-    agency: User
-  }
+#   type Pet {
+#     id: ID!
+#     name: String!
+#     species: String!
+#     breed: BreedsToPets[]
+#     savedBy: UsersToPets[]
+#     profile: PetProfile
+#     agency: User
+#   }
 
-  # enum Species {
-  #   CAT 
-  #   DOG
-  #   BIRD
-  #   HORSE
-  #   FISH
-  #   REPTILE
-  #   BARNYARD
-  # }
+#   # enum Species {
+#   #   CAT 
+#   #   DOG
+#   #   BIRD
+#   #   HORSE
+#   #   FISH
+#   #   REPTILE
+#   #   BARNYARD
+#   # }
 
-  type BreedsToPets {
-    breed: Breed
-    pet: Pet
-  }
+#   type BreedsToPets {
+#     breed: Breed
+#     pet: Pet
+#   }
 
-  type PetProfile {
-    id: ID!
-    age: Int
-    bio: String
-    weight: Int
-    birthday: String
-    isFixed: Boolean
-    isAdopted: Boolean!
-    isAvailable: Boolean!
-    isHouseTrained: Boolean
-    isVaccineCurrent: Boolean
+#   type PetProfile {
+#     id: ID!
+#     age: Int
+#     bio: String
+#     weight: Int
+#     birthday: String
+#     isFixed: Boolean
+#     isAdopted: Boolean!
+#     isAvailable: Boolean!
+#     isHouseTrained: Boolean
+#     isVaccineCurrent: Boolean
 
-    sex: String
-    coat: String 
-    diet: String
-    color: String
-    goodWith: String
-    personality: String
+#     sex: String
+#     coat: String 
+#     diet: String
+#     color: String
+#     goodWith: String
+#     personality: String
 
-    images: ImagesToPetProfiles[]
-    pet: Pet
-  }
+#     images: ImagesToPetProfiles[]
+#     pet: Pet
+#   }
 
-  type ImagesToPetProfiles {
-    image: Image
-    petProfile: PetProfile
-  }
+#   type ImagesToPetProfiles {
+#     image: Image
+#     petProfile: PetProfile
+#   }
 
-  type Image {
-    id: ID!
-    url: String
-    file: String
-    thumbnail: String
+#   type Image {
+#     id: ID!
+#     url: String
+#     file: String
+#     thumbnail: String
 
-    pets: ImagesToPetProfiles[]
-  }
+#     pets: ImagesToPetProfiles[]
+#   }
 
-  # enum Sex {
-  #   UNKNOWN
-  #   FEMALE
-  #   MALE
-  # }
+#   # enum Sex {
+#   #   UNKNOWN
+#   #   FEMALE
+#   #   MALE
+#   # }
 
-  # enum Color {
-  #   UNKNOWN
-  #   BLACK
-  #   WHITE
-  #   BROWN
-  #   GOLDEN
-  #   SPOTTED
-  #   BRINDLE
-  # }
+#   # enum Color {
+#   #   UNKNOWN
+#   #   BLACK
+#   #   WHITE
+#   #   BROWN
+#   #   GOLDEN
+#   #   SPOTTED
+#   #   BRINDLE
+#   # }
 
-  # enum Personality {
-  #   UNKNOWN
-  #   ACTIVE
-  #   CURIOUS
-  #   GOOFY
-  #   HYPER
-  #   LAZY
-  #   LONER
-  # }
+#   # enum Personality {
+#   #   UNKNOWN
+#   #   ACTIVE
+#   #   CURIOUS
+#   #   GOOFY
+#   #   HYPER
+#   #   LAZY
+#   #   LONER
+#   # }
 
-  # enum Diet {
-  #   STANDARD
-  #   MEDICAL
-  #   WEIGHT
-  # }
+#   # enum Diet {
+#   #   STANDARD
+#   #   MEDICAL
+#   #   WEIGHT
+#   # }
 
-  # enum Coat {
-  #   UNKNOWN
-  #   SHORT
-  #   MEDIUM
-  #   LONG
-  #   NONE
-  # }
+#   # enum Coat {
+#   #   UNKNOWN
+#   #   SHORT
+#   #   MEDIUM
+#   #   LONG
+#   #   NONE
+#   # }
 
-  # enum GoodWith {
-  #   UNKNOWN
-  #   CATS
-  #   DOGS
-  #   CHILDREN
-  #   CATS_AND_DOGS
-  #   ALL
-  # }
+#   # enum GoodWith {
+#   #   UNKNOWN
+#   #   CATS
+#   #   DOGS
+#   #   CHILDREN
+#   #   CATS_AND_DOGS
+#   #   ALL
+#   # }
 `;
